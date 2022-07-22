a11e25d84d7402e8df9e62aebd39795ae6651c4a
Haira Lei <darhaii13@gmail.com>
2022-07-22 22:51:03 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const cardImages = [
+  { src: "/assets/chimmy.png" },
+  { src: "/assets/cooky.png" },
+  { src: "/assets/koya.png" },
+  { src: "/assets/mang.png" },
+  { src: "/assets/rj.png" },
+  { src: "/assets/shooky.png" },
+  { src: "/assets/tata.png" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Game BT21</h1>
+      <button>New Game</button>
     </div>
   );
 }
